9afc09fb038309a91d21a7e49c7ea01de360c376
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-03-07 18:59:42 +0100

    add function checkEntry + wrongEntry + askLetter

--- pendu_js/js/pendu.js
+++ pendu_js/js/pendu.js
@@ -1,6 +1,7 @@
 //Challenge Pendu
 //--------------------
 
+//Game initialisation -----
 var wordsList = ['crayon', 'stylo', 'feutre', 'pointe', 'mine', 'gomme', 'dessin', 'coloriage', 'rayure', 'peinture', 'pinceau', 'couleur', 'craie', 'papier', 'feuille',
                 'cahier', 'carnet', 'carton', 'ciseaux', 'découpage', 'pliage', 'colle', 'affaire', 'casier', 'caisse', 'trousse', 'cartable', 'jouet', 'pion', 'domino']
 var wordIndex = 0;
@@ -10,26 +11,20 @@ var currentWord = "";
 var currentLetter = "";
 var tryLeft = 5;
 
-
-//Game initialisation
+//initialisation avec un rand de l'index pour la liste de mot
 wordIndex = randWordIndex(wordsList);
+//initialisation du mot à trouver
 wordToFind = wordsList[wordIndex];
+//initialisation du mot en cours de reflexion de l'utilisateur
 currentWord = initCurrentWord(wordToFind);
 
+//message de départ du jeu
 alert("Bienvenue dans le jeu du pendu, vous êtes pret?");
 
-//Game loop
+//Game loop -----
+//Demande à l'utilisateur une lettre
 currentLetter = askLetter();
-
-console.log("currentWord: " + currentWord);
-console.log("wordIndex: " + wordIndex);
-console.log("wordToFind: " + wordToFind);
-console.log("currentLetter: " + currentLetter);
-
-
-
-
-
+checkLetter(currentLetter);
 
 
 function randWordIndex(list){
@@ -45,11 +40,29 @@ function initCurrentWord(word){
     return initWord;
 }
 
-function updateCurrentWord(){
+function checkEntry(letter) {
+    if (letter.trim().length === 1) {
+        var char = letter.charAt(0);
+        return (/[a-zA-Z]/).test(char);
+    } else{        
+        return false;
+    }
+}
+
+function wrongEntry() {
+    alert("Mauvaise entrée ! Merci de taper une lettre uniquement :)");
+    askLetter();
+}
+
+function updateCurrentWord(letter){
 
 }
 
 function askLetter(){
     var letter = prompt("Mot à deviner: " + currentWord + " - Quel lettre voulez vous essayez? - " + tryLeft + " essais restant");
-    return letter;
+    if (checkEntry(letter)) {
+        return letter;
+    } else {
+        return wrongEntry();
+    }
 }
