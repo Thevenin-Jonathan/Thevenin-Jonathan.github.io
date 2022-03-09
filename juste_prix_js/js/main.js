e6c007b2bbb7d51f89d9beba052be04d1969e6db
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-03-09 18:03:10 +0100

    Add juste prix game

---
+++ juste_prix_js/js/main.js
@@ -0,0 +1,66 @@
+/*************************************************/
+/********************VARIABLES********************/
+/*************************************************/
+
+const game = {
+    nbToFind: 0,
+    userNb: 0,
+    tryNumber: 0,
+    min: 100,
+    max: 1000,
+    scores: []
+}
+
+/*************************************************/
+/********************GAME LOOP********************/
+/*************************************************/
+
+init();
+play();
+displayScore();
+
+/*************************************************/
+/********************FUNCTIONS********************/
+/*************************************************/
+
+function randNumber(min, max) {
+    let number = (Math.random() * (max - min)) + min;
+    number = Math.round(number);
+    console.log("nb: " + number);
+    return number;
+}
+
+function init() {
+    game.nbToFind = randNumber(game.min, game.max);
+    game.tryNumber = 0;
+    console.log(game.nbToFind);
+}
+
+function play() {
+    game.userNb = prompt(`Veuillez saisir un nombre entre ${game.min} et ${game.max} :`);
+    game.userNb = parseInt(game.userNb);
+    game.tryNumber++;
+    if (game.userNb < game.nbToFind) {
+        alert("Non, c'est plus !")
+        return play();
+    } else if (game.userNb > game.nbToFind) {
+        alert("Non, c'est moins !")
+        return play();
+    } else {
+        const s = game.tryNumber <= 1 ? "" : "s";
+        alert(`Bravo, vous avez trouvé en ${game.tryNumber} essai${s} !`)
+        game.scores.push(game.tryNumber);
+        if (confirm("Voulez vous rejouer ?")) {
+            init();
+            return play();
+        }
+    }
+}
+
+function displayScore() {
+    console.log("SCORES :")
+    for (let i = 0; i < game.scores.length; i++) {
+        const s = i === 0 ? "" : "s";
+        console.log(`Partie ${i + 1} : ${game.scores[i]} essai${s}`);
+    }
+}
